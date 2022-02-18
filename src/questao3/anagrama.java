005b9e3d4f7aaf7eec937fbfd7bb7a5b544b8527
unknown <pedrobmotta@hotmail.com>
2022-02-17 23:18:54 -0300

    Questão 3 - tentando fazer leitura no numero de vezes que letra repete.

--- src/questao3/anagrama.java
+++ src/questao3/anagrama.java
@@ -17,33 +17,33 @@ public class anagrama {
 	public static void main(String[] args) {
 		
 		String str;
-        int tamanho, i, j, cont = 0, v[];
+        int tamanho, i, j, cont = 1;
 		Scanner teclado = new Scanner(System.in);
         System.out.println("Digite uma palavra: ");
         
         str = teclado.next();
-        v[] = str.length();
+        int v[] = new int[str.length()];
+       
         for(i = 0; i < str.length(); i++) {
-        	for(j = 0; j < str.length(); j++) {
-        		if(i == j) {
-        			j++;
+        	for(j = i + 1; j < str.length(); j++) {
+        		if(i > 1) {
+        			for(int k = j + 1; k < str.length(); k++) {
+        				if(str.charAt(i) == str.charAt(k)) {
+        					cont--;
+        				}
+        			}
         		}
         		if(str.charAt(i) == str.charAt(j)) {
         			cont++;
         		}
+        		v[i] = cont;
         	}
-        	v[str.length()]
-        	
+        	cont = 1;
+        }
+        for(i=0; i<v.length; i++) {
+        	System.out.print(v[i]+" ");
         }
         
-        
-        
-        tamanho = str.length();
-        
-        
-       
-        
-
 	}
 
 }
