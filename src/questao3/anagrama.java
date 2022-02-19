e317373b5f1a789ba8ff622ecd53ee181fa254ae
unknown <pedrobmotta@hotmail.com>
2022-02-19 12:24:25 -0300

    Corrigindo a questão 3

--- src/questao3/anagrama.java
+++ src/questao3/anagrama.java
@@ -1,49 +1,60 @@
 package questao3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class anagrama {
 
-	private static long fatorialDoNumero(int num) {
-		int anterior = num-1;
-		long fatorialDesteNumero = 0;
-		
-		if (num <= 1) return 1;
-			else fatorialDesteNumero =  num * fatorialDoNumero(anterior); // Aqui está a recursividade no programa. O método "fatorialDoNumero" é chamado várias vezes dentro dele mesmo.
-		
-		return fatorialDesteNumero;
-	}
+public class anagrama {
 	
 	public static void main(String[] args) {
 		
-		String str;
-        int tamanho, i, j, cont = 1;
 		Scanner teclado = new Scanner(System.in);
-        System.out.println("Digite uma palavra: ");
         
-        str = teclado.next();
-        int v[] = new int[str.length()];
-       
-        for(i = 0; i < str.length(); i++) {
-        	for(j = i + 1; j < str.length(); j++) {
-        		if(i > 1) {
-        			for(int k = j + 1; k < str.length(); k++) {
-        				if(str.charAt(i) == str.charAt(k)) {
-        					cont--;
-        				}
-        			}
-        		}
-        		if(str.charAt(i) == str.charAt(j)) {
+		System.out.println("Digite uma string qualquer: ");
+		
+        String str = teclado.nextLine();
+        ArrayList<String> lista = new ArrayList<>();
+        int tamStr = str.length();
+        int cont = 0;
+        
+        for (int i = 0; i < tamStr; i++) {
+        	for (int letra = 1; letra <= tamStr-i; letra++) {
+        		String subStr = str.substring(i, i+letra);
+        		lista.add(subStr);
+            }
+        }
+        for (int i = 0; i < lista.size(); i++) {
+        	String elemento = lista.get(i);
+        	for (int j = i+1; j < lista.size(); j++) {
+        		if(eAnagrama(lista.get(j), elemento)){
         			cont++;
         		}
-        		v[i] = cont;
         	}
-        	cont = 1;
         }
-        for(i=0; i<v.length; i++) {
-        	System.out.print(v[i]+" ");
-        }
+         
+        System.out.println("A string digitada contem "+cont+" par(es) de anagrama(s).");
+        teclado.close();
         
+	}
+
+    public static boolean eAnagrama(String str1, String str2){
+        if(str1.length() != str2.length()){
+            return false;
+        }
+        int[] mapa = new int[26];
+        for(int i=0; i < str1.length(); i++){
+            char ch = str1.charAt(i);
+            mapa[ch - 'a']++;
+            ch = str2.charAt(i);
+            mapa[ch - 'a']--;
+        }
+
+        for(int i=0; i<mapa.length; i++){
+            if(mapa[i] != 0){
+                return false;
+            }
+        }
+        return true;
 	}
 
 }
